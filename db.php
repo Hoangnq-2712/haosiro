c5ed871b525a2165f2bad3a9fdbf0fd8b04509d0
Tranhao22 <67888938+Tranhao22@users.noreply.github.com>
2021-10-11 14:50:47 +0700

    update

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 	function query($sql){
 		//ket noi server
-		$conn=mysqli_connect("mysql5045.site4now.net","a79e2b_haosiro","hongnga24092000@") or die("lỗi kết nối");
-		mysqli_select_db($conn,"db_a79e2b_haosiro") or die("lỗi csdl");
+		$conn=mysqli_connect("mysql5045.site4now.net","a7af02_deploy","01633568287a@") or die("lỗi kết nối");
+		mysqli_select_db($conn,"db_a7af02_deploy") or die("lỗi csdl");
 		//thuc hiện truy vấn
 		$result=mysqli_query($conn,$sql) or die("lỗi truy vấn");
 		//đóng kết nối
